5aa25080c4e31f3b37c345ea2750349f2183575f
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-17 11:57:26 +0530

    added random() to get number 1 to 6

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,6 +1,15 @@
+import java.util.Random;
 
 public class SnakeNLadder {
 	int player_position=0;
+	public int rollDice()
+	{
+		int n = 0;
+		Random r = new Random();
+		n=r.nextInt(7);
+		return (n==0?1:n);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
 		
