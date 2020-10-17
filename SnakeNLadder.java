4d5cc1be7bf11f6a55d0968eefadf32a10501468
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-17 13:37:13 +0530

    counted number of times dice rolled

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class SnakeNLadder {
 	int player_position=0;
@@ -10,41 +11,47 @@ public class SnakeNLadder {
 		n=r.nextInt(7);
 		return (n==0?1:n);
 	}
-	
+
+
+
 	public void checkOption() {
 		Random ran =new Random();
+		int count=0;
 		while(true) {
-		int option =ran.nextInt(3);
-		System.out.println(option);
-		if(option==0) 
-			System.out.println("No Play stay in the same position");
-		if(option==1)
-			System.out.println("Ladder");
-		player_position=player_position+rollDice();
-		if(option==2)	
-			System.out.println("Snake");
-		player_position=player_position-rollDice();
-		
-		if(player_position<=0)
-			player_position=0;
-		int previous_position = 0;
-		if(player_position>100)
-			player_position=previous_position;
-			
-		if(player_position==wining_Position)
-			System.out.println("Player reach winning position");
-		break;
-			
+			int option =ran.nextInt(3);
+			System.out.println(option);
+			if(option==0) 
+				System.out.println("No Play stay in the same position");
+			if(option==1)
+				System.out.println("Ladder");
+			player_position=player_position+rollDice();
+			count++;
+			if(option==2)	
+				System.out.println("Snake");
+			player_position=player_position-rollDice();
+			count++;
+
+			if(player_position<=0)
+				player_position=0;
+			int previous_position = 0;
+			if(player_position>100)
+				player_position=previous_position;
+
+			if(player_position==wining_Position)
+				System.out.println("Player reach winning position");
+			break;
+
 		}
+		System.out.println("number of times dice was rolles"+count);
 
 
 
 	}
-	
+
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
-		
+
 
 
 
