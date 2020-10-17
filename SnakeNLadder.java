3b18fb6b8f70fdc527945030001887241f36de36
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-17 12:40:22 +0530

    Added while loop to repeat till player reaches winning position

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class SnakeNLadder {
 	int player_position=0;
+	int wining_Position=100;
 	public int rollDice()
 	{
 		int n = 0;
@@ -9,8 +10,10 @@ public class SnakeNLadder {
 		n=r.nextInt(7);
 		return (n==0?1:n);
 	}
+	
 	public void checkOption() {
 		Random ran =new Random();
+		while(true) {
 		int option =ran.nextInt(3);
 		System.out.println(option);
 		if(option==0) 
@@ -21,10 +24,18 @@ public class SnakeNLadder {
 		if(option==2)	
 			System.out.println("Snake");
 		player_position=player_position-rollDice();
+		
+		if(player_position<=0)
+			player_position=0;
+		if(player_position==wining_Position)
+			System.out.println("Player reach winning position");
+			
+		}
 
 
 
 	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
