0a30b93e27006a5d876c5f6e8b9cfdf760883c9a
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-17 12:15:35 +0530

    added Added case statement to check noplay,ladder and snake options

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -9,11 +9,29 @@ public class SnakeNLadder {
 		n=r.nextInt(7);
 		return (n==0?1:n);
 	}
-	
+	public void checkOption() {
+		Random ran =new Random();
+		int option =ran.nextInt(3);
+		System.out.println(option);
+		if(option==0) 
+			System.out.println("No Play stay in the same position");
+		if(option==1)
+			System.out.println("Ladder");
+		player_position=player_position+rollDice();
+		if(option==2)	
+			System.out.println("Snake");
+		player_position=player_position-rollDice();
+
+
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
 		
-		
+
+
+
 	}
 
 }
